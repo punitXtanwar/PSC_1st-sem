9b34a0f039b3607faaff655502208bac7d9c36b4
punitXtanwar <punittanwar565@gmail.com>
2024-11-16 11:24:31 +0530

    Add files via upload
    November 16

---
+++ 55555.py
@@ -0,0 +1,49 @@
+# n = 10
+# sum_natural_numbers = n * (n + 1) // 2
+# print("sum of first", n, "natural numbers is:", sum_natural_numbers )
+
+
+# n = 1234567890
+
+# sum_natural_numbers = 0
+
+# for i in range (1, n+1):
+#     sum_natural_numbers += i
+
+# print("sum of first", n, "natural numbers is:", sum_natural_numbers)    
+
+
+
+# n = 6
+# even_sum = 0
+# odd_sum = 0
+
+# for i in range (1 , n + 1):
+#     if i % 2 == 0:
+#         even_sum += i
+#     else:
+#         odd_sum += i
+
+# print("sum of even numbers :", even_sum)
+# print("sum of odd numbers :", odd_sum)
+
+
+
+# n = 11
+# even = n//2
+# even_sum= even * (even +1)
+
+# odd= (n+1)//2
+# odd_sum = odd * odd
+
+# print("sum of even numbers:", even_sum)
+# print("sum of odd numbers:", odd_sum)
+
+
+# n = 12
+# print("divisors of", n, "are:")
+# for i in range (1, n+1):
+#     if n%i == 0:
+#         print(i)
+
+
