7373e20efb0b2a302743a7706d89a2f2346b2206
punitXtanwar <punittanwar565@gmail.com>
2024-11-22 09:28:18 +0530

    Add files via upload

---
+++ 4240058.py
@@ -0,0 +1,53 @@
+print("Hello")
+
+
+# Age= int(input("Enter the Age:"))
+# if Age < 18:
+#     print("TeenAger")
+# elif 18 <= Age < 30:
+#     print("Adult")
+# else:
+#     print("senior")
+
+
+
+
+# x = int(input("enter the Age:"))
+# y = int(input("enter the Age:"))
+# z = int(input("enter the Age:"))
+# if x < 18:
+#     print("teenAger")
+# if 18 <= y <30:
+#     print("adult")
+# if z > 30: 
+#     print("senior")
+
+
+
+
+
+
+
+person1= int(input("Enter a Number"))
+person2= int(input("Enter a Number"))
+person3= int(input("Enter a Number"))
+if person1 < 18 :
+    print(" person1 is Teenager")
+if person1 >= 18 and person2 <= 30 :
+    print("person1 is Adult")
+if person1 > 30 :
+    print("person1 is Senior")
+
+if person2 < 18 :
+    print("person2 is Teenager")
+if person2 >= 18 and person2 <= 30 :
+    print("person2 is Adult")
+if person2 > 30 :
+    print("person2 is Senior")    
+            
+if person3 < 18 :
+    print("person3 is Teenager")
+if person3 >= 18 and person2 <= 30 :
+    print("person3 is Adult")
+if person3 > 30 :
+    print("person3 is Senior")
